dfe99d976d28915a1e8eed0d87ae23755ea62bd0
tomclaine <84493862+tomclaine@users.noreply.github.com>
2023-02-09 16:41:33 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+//Bai 1
+// function input(mark){
+//     if(mark>=85){
+//         console.log("A");
+//     }
+//     else if(mark>=70 && mark<85){
+//         console.log("B");
+//     }
+//     else if(mark>=40 && mark<70){
+//         console.log("C");
+//     }
+//     else{
+//         console.log("D");
+//     }
+// }
+// input(39);
+
+
+//Bai 2
+// function compare(a,b){
+//     if(a>b){
+//         console.log(a);
+//     }
+//     else if(a<b){
+//         console.log(b);
+//     }
+//     else{
+//         alert("2 so bang nhau");
+//     }
+// }
+// compare(10,6);
+
+
+//Bai 3
+// function check(number){
+//     if(number>0){
+//         console.log(`${number} la so duong`);
+//     }
+//     else if(number<0){
+//         console.log(`${number} la so am`);
+//     }
+//     else{
+//         console.log("la so 0");
+//     }
+// }
+// check(0);
+
+
+//Bai 4
+// function check(number){
+//     if(number%2==0){
+//         console.log(`${number} la so chan`);
+//     }
+//     else{
+//         console.log(`${number} la so le`);
+//     }
+// }
+// check(6);
+
+
+//Bai 5
+// function check(number){
+//     if(number%3==0 && number%5==0){
+//         console.log(`${number} chia het cho ca 3 va 5`);
+//     }
+//     else if(number%3==0 || number%5==1){
+//         console.log(`${number} chia het cho 3 nhung khong chia het cho 5`);
+//     }
+//     else if(number%5==0 || number%3==1){
+//         console.log(`${number} chia het cho 5 nhung khong chia het cho 3`);
+//     }
+//     else{
+//         console.log(`${number} khong chia het cho ca 3 va 5`);
+//     }
+// }
+// check(33);
+
+
+//Bai 6
+// function check(a,b,c){
+//     if(c==a+b){
+//         console.log(`${c} = ${a} + ${b}`);
+//     }
+//     else{
+//         console.log(`${c} != ${a} + ${b}`);
+//     }
+// }
+// check(1,2,3);
